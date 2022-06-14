a0e59b99cad1f5b1bebd48a4cbfca6abd51d0138
Aman Tripathi <imamantripathi@gmail.com>
2022-06-14 19:36:23 +0530

    Stack implementation using Array

--- Stack_&_Queue/stack.c
+++ Stack_&_Queue/stack.c
@@ -6,26 +6,25 @@ int stack[5];
 int top = -1;
 
 int isEmpty() { return (top == -1) ? 1 : 0; }
-int isFull() { return (top == Max-1) ? 1 : 0; }
+int isFull() { return (top == Max - 1) ? 1 : 0; }
 
 void push(int data)
 {
-    if (isFull()==1)
+    if (isFull() == 1)
     {
-         printf("Overflow");
-      
+        printf("Overflow");
     }
-    else{
-          top += 1;
+    else
+    {
+        top += 1;
         stack[top] = data;
         printf("Push Successfully");
     }
-       
 }
 int pop()
 {
     int temp;
-    if (isEmpty()==0)
+    if (isEmpty() == 0)
     {
         temp = stack[top];
         top -= 1;
@@ -36,7 +35,7 @@ int pop()
 }
 void peek()
 {
-    if (isEmpty()==0)
+    if (isEmpty() == 0)
     {
         printf("%d", stack[top]);
     }
@@ -45,7 +44,7 @@ void peek()
 }
 void display()
 {
-    if (isEmpty()==0)
+    if (isEmpty() == 0)
     {
         for (int i = top; i >= 0; i--)
         {
@@ -55,52 +54,55 @@ void display()
 }
 void main()
 {
-    int input; int data;
-    while(1){
-    printf(" \n0. Exit \n");
-    printf("1. Push \n");
-    printf("2. Pop \n");
-    printf("3. peek \n");
-    printf("4. Display \n");
-    printf("5. Check Empty or not \n");
-    printf("6. Check Full or not \n");
-    scanf("%d", &input);
-    switch (input)
+    int input;
+    int data;
+    while (1)
     {
-        case 0: exit(0);
-        break;
-    case 1:
-        printf("Enter data to push: ");
-        scanf("%d", &data);
-        push(data);
-        break;
-    case 2:
-        printf("Deleted element is %d.", pop());
-        break;
-    case 3:
-        peek();
-        break;
-    case 4:
-        display();
-        break;
-    case 5:
-        if (isEmpty()==1)
-            printf("Stack is Empty");
-        else
-            printf("Stack is not Empty");
-        break;
-    case 6:
-    if (isFull()==1)
-    {
-        printf("Stack is full");
+        printf(" \n0. Exit \n");
+        printf("1. Push \n");
+        printf("2. Pop \n");
+        printf("3. peek \n");
+        printf("4. Display \n");
+        printf("5. Check Empty or not \n");
+        printf("6. Check Full or not \n");
+        scanf("%d", &input);
+        switch (input)
+        {
+        case 0:
+            exit(0);
+            break;
+        case 1:
+            printf("Enter data to push: ");
+            scanf("%d", &data);
+            push(data);
+            break;
+        case 2:
+            printf("Deleted element is %d.", pop());
+            break;
+        case 3:
+            peek();
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            if (isEmpty() == 1)
+                printf("Stack is Empty");
+            else
+                printf("Stack is not Empty");
+            break;
+        case 6:
+            if (isFull() == 1)
+            {
+                printf("Stack is full");
+            }
+            else
+                printf("Stack is not full");
+
+            break;
+        default:
+            printf("Wrong input");
+            break;
+        }
     }
-    else
-    printf("Stack is not full");
-     
-    break;
-    default:
-    printf("Wrong input");
-        break;
-    }
-}
 }
